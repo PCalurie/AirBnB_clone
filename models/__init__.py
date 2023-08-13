ae2c89d5c182e1367b59f5abd5183a9dc770cf50
Enoshi4072 <enockkibet6642@gmail.com>
2023-08-13 14:09:28 +0000

    0. README

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Initializes the package with serialized data
-and reloads it in the application memory"""
-
-from models.engine.file_storage import FileStorage
-storage = FileStorage()
-storage.reload()
